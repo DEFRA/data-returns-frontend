accc0af90c61b599e74751dd962d67cdfa8da88c
Neil Hathaway <root@neil-VirtualBox>
2016-02-04 10:04:55 +0000

    Multiple error pages updated to split missing and incorrect errors into separate handlebars views.

--- app/routeHandlers/O210Handler.js
+++ app/routeHandlers/O210Handler.js
@@ -1,7 +1,6 @@
 
 var config = require('../config/configuration_' + (process.env.NODE_ENV || 'local'));
 var cacheHandler = require('../lib/cache-handler');
-
 module.exports = {
   getHandler: function (request, reply) {
 
@@ -10,22 +9,20 @@ module.exports = {
     var sessionID = 'id_' + request.session.id;
     var filename;
     var filekey = sessionID + '_SourceName';
-
     filekey = sessionID + '_SourceName';
     cacheHandler.getValue(filekey)
       .then(function (filename) {
         filename = filename.replace(/"/g, "");
-
         cacheHandler.getValue(key)
           .then(function (result) {
 
             result = JSON.parse(result);
-
             //get the first error and extract basic error details
             var firstError = result[0];
             var rowText = '';
             var item;
-
+            var errorType = firstError.errorColumnText;
+            
             for (var i = 0; i < result.length; i++) {
               item = result[i];
               rowText += rowText !== '' ? ',' + item.rowNumber : item.rowNumber;
@@ -37,7 +34,10 @@ module.exports = {
               columnName: firstError.columnName,
               errorMessage: firstError.errorMessage,
               rows: rowText,
-              errorType: firstError.errorColumnText
+              errorType: errorType,
+              isMissing: (errorType.toLowerCase() === 'missing') ? true : false,
+              isIncorrect: (errorType.toLowerCase() === 'incorrect') ? true : false,
+              data: result
             });
           })
           .catch(function (err) {
@@ -47,7 +47,6 @@ module.exports = {
       .catch(function (err) {
         console.error('Error getting ' + key, err);
       });
-
   },
   postHandler: function (request, reply) {
 
