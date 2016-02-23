8d5e8ae8ecebacaf8f5f0dd1ce0ba37cf89c6bcb
Neil Hathaway <root@neil-VirtualBox>
2016-02-23 15:03:32 +0000

    Changed /index for /01-start/01-start page

--- app/routeHandlers/indexHandler.js
+++ app/routeHandlers/indexHandler.js
@@ -4,5 +4,5 @@ var config = require('../config/configuration_' + (process.env.NODE_ENV || 'loca
 // redirect to index if on a local dev machine
 module.exports.redirectToIndex = function (request, reply) {
   var location = (config.env && config.env === 'local') ? '/index' : '/01-start/01-start';
-  reply.redirect(location);
+  reply.redirect('/01-start/01-start');
 };
