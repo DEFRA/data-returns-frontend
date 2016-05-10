d0923e4e360230255953be5e3748b6dd4b0dc74c
Jeremy Gillies <jeremygillies@gmail.com>
2016-05-10 10:21:34 +0100

    added aria disabled to browse file button

--- assets/javascripts/fancy-file-upload.js
+++ assets/javascripts/fancy-file-upload.js
@@ -25,6 +25,7 @@ $(document).on('change', '.btn-file :file', function () {
 $("#uploadForm").submit(function () {
   $(".checking").show(0);
   $('#check-for-errors-btn').attr('disabled', 'disabled');
+  $('#check-for-errors-btn').attr('aria-disabled', true);
 });
 
 $(document).ready(function () {
@@ -32,8 +33,9 @@ $(document).ready(function () {
   $('.button-choose-file').addClass("btn btn-default btn-file button");
   $('.browse-text').show();
   $('#check-for-errors-btn').attr('disabled', 'disabled');
+  $('#check-for-errors-btn').attr('aria-disabled', true);
   $('#file-select-button').attr('type','file');
- 
+
 
   var existing = $('.btn-file :file').val();
 
@@ -46,6 +48,7 @@ $(document).ready(function () {
     $('.file-label').html(existing);
     $('.del-label').html('<a id="remove-link" href="#">Remove</a>');
     $('#check-for-errors-btn').removeAttr('disabled');
+    $('#check-for-errors-btn').removeAttr('aria-disabled', true);
     $('#file-select-button').hide();
     $('.fancy-file-button').css({'-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=50)',
       'filter': 'alpha(opacity=50)',
@@ -53,9 +56,10 @@ $(document).ready(function () {
       '-khtml-opacity': '0.5',
       'opacity': '0.5'});
     $('#remove-link').click(function () {
-      
+
       $('#statusbar-header').hide();
       $('#check-for-errors-btn').attr('disabled', 'disabled');
+      $('#check-for-errors-btn').attr('aria-disabled', true);
       $(".checking").hide(0);
       $('#file-select-button').show();
       $('.fancy-file-button').css({'-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=100)',
@@ -79,13 +83,14 @@ $(document).ready(function () {
         $('#title').html('Confirm the file you want to send');
       }
       $('#check-for-errors-btn').removeAttr('disabled');
+      $('#check-for-errors-btn').removeAttr('aria-disabled', true);
       $('#file-select-button').hide();
       $('.fancy-file-button').css({'-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=50)',
         'filter': 'alpha(opacity=50)',
-        '-moz-opacity': '0.5',  
+        '-moz-opacity': '0.5',
         '-khtml-opacity': '0.5',
         'opacity': '0.5'});
-      
+
       $('#remove-link').click(function () {
         $('#statusbar-header').hide();
         $('#loading-image').hide();
@@ -96,6 +101,7 @@ $(document).ready(function () {
         }
         $('#title').html('Choose the file you want us to check');
         $('#check-for-errors-btn').attr('disabled', 'disabled');
+        $('#check-for-errors-btn').attr('aria-disabled', true);
         $('#file-select-button').show();
         $('.fancy-file-button').css({'-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=100)',
           'filter': 'alpha(opacity=100)',
