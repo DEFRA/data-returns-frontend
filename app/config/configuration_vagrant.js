8703e5fe0ccd3a1180b0a1901820593c3aab8a6a
graham-willis-druid <graham.willis.ea@gmail.com>
2016-06-06 14:22:54 +0100

    Add configuration file for the vagrant implmentation

---
+++ app/config/configuration_vagrant.js
@@ -0,0 +1,82 @@
+'use strict';
+/*
+ * Settings for your local machine environment.
+ */
+var config = require('./configuration_global');
+// Set a meaningful name for this environment.
+config.env = 'local';
+config.sessionStorage.mode = 'redis';
+/* Local SMTP Configuration */
+config.smtp = config.smtp || {};
+config.smtp.host = 'smtp.sendgrid.net';
+config.smtp.port = 25;
+config.smtp.ignoreTLS = true;
+config.smtp.username = process.env.smtpuser;
+config.smtp.password = process.env.smtppw;
+config.smtp.fromEmailAddress = 'noreply-local@environment-agency.gov.uk';
+config.smtp.useFooter = false;
+// mail catcher config
+// Set config.smtp.useMailCatcher = true; to use http://mailcatcher.me/
+config.smtp.useMailCatcher = true; // set to false on AWS servers
+config.smtp.mailcatcher.host = '127.0.0.1';
+config.smtp.mailcatcher.port = 1025;
+config.smtp.mailcatcher.ignoreTLS = true;
+//Support Contact details on emails
+config.smtp.support = {
+  email: 'enquiries@environment-agency.gov.uk',
+  UKPhone: '03708 506 506',
+  PhoneFromAbroad: '00 44 1709 389 201',
+  MiniCommNumber: '03702 422 549'
+};
+/* cache configuration */
+config.redis = {
+  clientOptions: {
+    host: 'localhost',
+    port: 6379
+  }
+};
+// CSV Validation
+config.CSV = {
+  validate: true,
+  VIRUS_SCAN: false,
+  maxfilesize: 2 * Math.pow(2, 20) //2MB
+};
+// API Endpoints
+
+var BASEURL = {
+  PROTOCOL: 'http://',
+  SERVER: '192.168.33.11:',
+  PORT: 9020
+};
+config.API = {};
+config.API.endpoints = {
+  'FILEUPLOAD': BASEURL.PROTOCOL + BASEURL.SERVER + BASEURL.PORT + '/data-exchange/upload',
+  'FILEUPLOADCOMPLETE': BASEURL.PROTOCOL + BASEURL.SERVER + BASEURL.PORT + '/data-exchange/complete',
+  'ERRORCHECKING': 'error_checking',
+  'ERRORSENDING': 'error_sending'
+};
+config.API.STATUS_CODES = {
+  OK: 200
+};
+//DEP specific configuration
+config.dep = {
+  returnTypeRulesLink: 'http://data-returns-help.herokuapp.com'
+};
+// Google Analytics configuration.
+config.useGoogleAnalytics = true;
+config.googleTagManagerId = process.env.DRF_TAG_MANAGER_ID || 'GTM-TEST';
+config.compressCSS = false;
+
+//errbit integration config
+config.errbit = {};
+config.errbit.options = {
+  enabled: false,
+  apiKey: process.env.ERRBIT_API_KEY,
+  appUrl: 'https://localhost:3000/start',
+  appName: 'Data-Returns Front End (AWS LOCAL)',
+  projectRoot: '/data-returns-frontend',
+  errBitServerURI: 'https://errbit.envage.co.uk/notifier_api/v2/notices'
+};
+
+// Publish the configuration.
+module.exports = config;
