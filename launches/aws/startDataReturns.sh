a4e56e7af79fa841c2b79ab5c232cf751bbac5ba
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-08-19 14:07:45 +0100

    Updated startup script to create required folders

--- launches/aws/startDataReturns.sh
+++ launches/aws/startDataReturns.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-cd /srv/data-returns
-source ~ubuntu/.data-returns/.dr_env
+
+ENV_VARS_FILE="~ubuntu/.data-returns/.dr_env"
+SERVICE_HOME="/srv/data-returns"
+LOG_DIR="${SERVICE_HOME}/logs"
+
+source ${ENV_VARS_FILE}
+
+cd ${SERVICE_HOME}
+
+# Install node modules
 npm install
+
+# Create log folder
+if [ ! -d ${LOG_DIR} ]; then
+    mkdir ${LOG_DIR}
+fi
 echo "http_proxy= https_proxy= node server.js" > /srv/data-returns/data-returns.sh
 chmod u+x /srv/data-returns/data-returns.sh
 pm2 start data-returns.sh
