4f9299cbf9ae616d7e938c91873b3a5bb5d03c26
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-09-09 13:38:38 +0100

    Move links from BE to file.

---
+++ config.js
@@ -0,0 +1,13 @@
+//var config = require('./app/config/configuration_' + (process.env.NODE_ENV || 'local'));
+
+//var json = JSON.stringify(config, null, 4);
+
+//var fs = require('fs');
+//fs.writeFile("./app/config/configuration_" + (process.env.NODE_ENV || "local") + ".json", json, function(err) {
+//    if(err) {
+//        return console.log(err);
+//    }
+
+//    console.log("The file was saved!");
+//});
+
