d08f653b2fe229cf06bf64a67e64d121c1938123
Jeremy Gillies <jeremygillies@gmail.com>
2017-02-24 14:38:29 +0000

    Added feedback link href

--- app/config/dep-help-links.js
+++ app/config/dep-help-links.js
@@ -21,6 +21,7 @@ module.exports.links = {
     Cookie_Guidance: 'http://www.aboutcookies.org/how-to-control-cookies/',
     Cookie_Page: '/guidance/cookies.html',
     Start_Page: '/start',
+    Done_Page: 'https://www.gov.uk/done/report-landfill-data',
     fields: {
         Rtn_Type: "/display-list?list=Rtn_Type",
         Rtn_Period: "/display-list?list=Rtn_Period",
